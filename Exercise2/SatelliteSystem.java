61e27e63bd3c2ffa99cf2f37151a14b829071079
NIVETHA0206 <nivetha777m@gmail.com>
2025-09-28 11:32:51 +0530

    Update SatelliteSystem.java

--- Exercise2/SatelliteSystem.java
+++ Exercise2/SatelliteSystem.java
@@ -2,6 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+/* Satellite Command System Programming Exercise */
 package SatelliteSystem;
 
 /**
